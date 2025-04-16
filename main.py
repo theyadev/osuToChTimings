3234d43ca36a0fb87c3580dbc9071bbb912aaec7
Théo Fassi <tfa@subteno.com>
2025-04-16 12:30:26 +0200

    chore: format

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os 
-
 with open("./osu.txt", "r") as file:
     content = file.read()
 
@@ -12,15 +10,13 @@ content = list(filter(lambda x: x.split(",")[-2] == "1", content))
 tickRate = 192
 
 # [Ticks, BPM, Signature, Minutes]
-chTimingLines = [
-    [0, 120, 4, 0]
-]
+chTimingLines = [[0, 120, 4, 0]]
 
 for line in content:
     timing, beatLength, signature, _, _, _, _, _ = line.split(",")
     bpm = round(60000 / float(beatLength))
-    print(f"Timing: {timing}, Beat Length: {beatLength} ms, Time Signature: {signature}, BPM: {bpm:.2f}")
-    
+    # print(f"Timing: {timing}, Beat Length: {beatLength} ms, Time Signature: {signature}, BPM: {bpm:.2f}")
+
     minutes = int(timing) / 60000
     minutesElapsed = minutes - chTimingLines[-1][-1]
     ticksElapsed = round(minutesElapsed * chTimingLines[-1][1] * tickRate)
@@ -28,15 +24,8 @@ for line in content:
     print(f"Minutes Elapsed: {minutesElapsed}, Ticks Elapsed: {ticksElapsed}, Ticks: {ticks}")
     chTimingLines.append([ticks, bpm, signature, minutes])
 
-print(chTimingLines)
-
-# Write to CloneHero.txt with proper formatting
 with open("CloneHero.txt", "w") as file:
     for line in chTimingLines:
         ticks, bpm, signature, _ = line
-        # Format: [ticks] = TS [Signature]
         file.write(f"{ticks} = TS {signature}\n")
-        # Format: [TICKS] = B [BPM]000
         file.write(f"{ticks} = B {int(bpm)}000\n")
-
-
